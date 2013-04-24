68696f765ceb3e6384aa49e08a31f5d4e101fb2e
di7chro <crille@crille.org>
2013-04-24 12:24:31 +0200

    Skalet på plats

---
+++ src/org/crille/hvdagen/MinDag.java
@@ -0,0 +1,14 @@
+package org.crille.hvdagen;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MinDag extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mindag);
+    }
+
+}
---
+++ src/org/crille/hvdagen/Start.java
@@ -0,0 +1,50 @@
+package org.crille.hvdagen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Start extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+
+        Button btnMindag = (Button) findViewById(R.id.start_btn_mindag);
+        Button btnSchema = (Button) findViewById(R.id.start_btn_schema);
+        Button btnTwitter = (Button) findViewById(R.id.start_btn_twitter);
+        Button btnLogin = (Button) findViewById(R.id.start_btn_login);
+
+        btnMindag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startMindag = new Intent(getApplicationContext(), MinDag.class);
+                startActivity(startMindag);
+            }
+        });
+        btnSchema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startSchema = new Intent(getApplicationContext(), Schema.class);
+                startActivity(startSchema);
+            }
+        });
+        btnTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startTwitter = new Intent(getApplicationContext(), Twitter.class);
+                startActivity(startTwitter);
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startLogin = new Intent(getApplicationContext(), Login.class);
+                startActivity(startLogin);
+            }
+        });
+    }
+}
